009d06e9d71cd46a069d75ff9ac31c898992a7ce
kamilsa <kamilsa16@gmail.com>
2017-12-05 21:00:42 +0300

    Add stateless validation test, add documentation
    Signed-off-by: kamilsa <kamilsa16@gmail.com>

--- test/module/shared_model/backend_proto/shared_proto_transaction_test.cpp
+++ test/module/shared_model/backend_proto/shared_proto_transaction_test.cpp
@@ -33,6 +33,43 @@ TEST(ProtoTransaction, Create) {
 }
 
 /**
+ * generates sample transaction without commands
+ * @return generated transaction
+ */
+iroha::protocol::Transaction generateEmptyTransaction() {
+  uint64_t created_time = 10000000000ull;
+  shared_model::interface::Transaction::TxCounterType tx_counter = 1;
+  std::string account_id = "admin@test", asset_id = "coin#test",
+              amount = "10.00";
+
+  iroha::protocol::Transaction proto_tx;
+  auto &payload = *proto_tx.mutable_payload();
+  payload.set_tx_counter(tx_counter);
+  payload.set_creator_account_id(account_id);
+  payload.set_created_time(created_time);
+
+  return proto_tx;
+}
+
+/**
+ * Helper function to generate AddAssetQuantityCommand
+ * @param account_id
+ * @param asset_id
+ * @return AddAssetQuantity protocol command
+ */
+iroha::protocol::AddAssetQuantity generateAddAssetQuantity(
+    std::string account_id, std::string asset_id) {
+  iroha::protocol::AddAssetQuantity command;
+
+  command.set_account_id(account_id);
+  command.set_asset_id(asset_id);
+  command.mutable_amount()->mutable_value()->set_fourth(1000);
+  command.mutable_amount()->set_precision(2);
+
+  return command;
+}
+
+/**
  * @given transaction field values and sample command values, reference tx
  * @when create transaction with sample command using transaction builder
  * @then transaction is built correctly
@@ -43,16 +80,11 @@ TEST(ProtoTransaction, Builder) {
   std::string account_id = "admin@test", asset_id = "coin#test",
               amount = "10.00";
 
-  iroha::protocol::Transaction proto_tx;
-  auto &payload = *proto_tx.mutable_payload();
-  auto &command = *payload.add_commands()->mutable_add_asset_quantity();
-  payload.set_tx_counter(tx_counter);
-  payload.set_creator_account_id(account_id);
-  payload.set_created_time(created_time);
-  command.set_account_id(account_id);
-  command.set_asset_id(asset_id);
-  command.mutable_amount()->mutable_value()->set_fourth(1000);
-  command.mutable_amount()->set_precision(2);
+  iroha::protocol::Transaction proto_tx = generateEmptyTransaction();
+  auto command =
+      proto_tx.mutable_payload()->add_commands()->mutable_add_asset_quantity();
+
+  command->CopyFrom(generateAddAssetQuantity(account_id, asset_id));
 
   auto tx = shared_model::proto::TransactionBuilder()
                 .txCounter(tx_counter)
@@ -63,4 +95,32 @@ TEST(ProtoTransaction, Builder) {
   auto &proto = tx.getTransport();
 
   ASSERT_EQ(proto_tx.SerializeAsString(), proto.SerializeAsString());
+}
+
+/**
+ * @given transaction field values and sample command values with wrongly set
+ * values
+ * @when create transaction with sample command using transaction builder
+ * @then transaction throws exception due to badly formed fields in commands
+ */
+TEST(ProtoTransaction, BuilderWithInvalidTx) {
+  uint64_t created_time = 10000000000ull;
+  shared_model::interface::Transaction::TxCounterType tx_counter = 1;
+  std::string account_id = "admintest"; // account_id without @
+  std::string asset_id = "coin#test", // asset_id without #
+      amount = "10.00";
+
+  iroha::protocol::Transaction proto_tx = generateEmptyTransaction();
+  auto command =
+      proto_tx.mutable_payload()->add_commands()->mutable_add_asset_quantity();
+
+  command->CopyFrom(generateAddAssetQuantity(account_id, asset_id));
+
+  ASSERT_THROW(shared_model::proto::TransactionBuilder()
+                   .txCounter(tx_counter)
+                   .creatorAccountId(account_id)
+                   .assetQuantity(account_id, asset_id, amount)
+                   .createdTime(created_time)
+                   .build(),
+               std::invalid_argument);
 }
